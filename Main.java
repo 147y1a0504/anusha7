ad32d8cb35f60d31c7f595be11d3ace4250a7c80
147y1a0504 <31848961+147y1a0504@users.noreply.github.com>
2017-09-13 13:29:15 +0530

    no compilation errors of main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s=new Student();
+		Student s1=new Student();
+		s.student(504,"Anusha",1996/12/27,70.90);
+		s1.student(503,"Alekhya",1996/04/12,72.23);
+		s.hashcode();
+		s.equals(Object s);
+		s.compareTo(Object s1);
+	}
+
+}
